0200765faefa2609fa853f9499a14bfc948a749b
Sam Thorogood <sam.thorogood@gmail.com>
2015-02-24 13:20:06 +1100

    gofmt, style updates, renamed Directions => Get

--- directions/directions.go
+++ directions/directions.go
@@ -19,241 +19,237 @@
 package directions // import "google.golang.org/maps/directions"
 
 import (
-    "net/http"
-    "encoding/json"
-    "log"
+	"encoding/json"
+	"log"
+	"net/http"
 
-    "google.golang.org/maps/internal"
+	"google.golang.org/maps/internal"
 
-    "golang.org/x/net/context"
+	"golang.org/x/net/context"
 )
-
 
 // Response represents a Directions API response.
 type Response struct {
-    // Routes lists the found routes between origin and destination.
-    Routes []Route
+	// Routes lists the found routes between origin and destination.
+	Routes []Route
 
-    // Status contains the status of the request, and may contain 
-    // debugging information to help you track down why the Directions 
-    // service failed. 
-    // See https://developers.google.com/maps/documentation/directions/#StatusCodes
-    Status string
+	// Status contains the status of the request, and may contain
+	// debugging information to help you track down why the Directions
+	// service failed.
+	// See https://developers.google.com/maps/documentation/directions/#StatusCodes
+	Status string
 }
 
 // Route represents a single route between an origin and a destination.
 type Route struct {
-    // Summary contains a short textual description for the route, suitable for 
-    // naming and disambiguating the route from alternatives.
-    Summary string `json:"summary"`
+	// Summary contains a short textual description for the route, suitable for
+	// naming and disambiguating the route from alternatives.
+	Summary string `json:"summary"`
 
-    // Legs contains information about a leg of the route, between two locations within the
-    // given route. A separate leg will be present for each waypoint or destination specified. 
-    // (A route with no waypoints will contain exactly one leg within the legs array.)
-    Legs []Leg `json:"legs"`
+	// Legs contains information about a leg of the route, between two locations within the
+	// given route. A separate leg will be present for each waypoint or destination specified.
+	// (A route with no waypoints will contain exactly one leg within the legs array.)
+	Legs []*Leg `json:"legs"`
 
-    // WaypointOrder contains an array indicating the order of any waypoints in the calculated route
-    WaypointOrder []int `json:"waypoint_order"`
+	// WaypointOrder contains an array indicating the order of any waypoints in the calculated route.
+	WaypointOrder []int `json:"waypoint_order"`
 
-    // OverviewPolyline contains an approximate (smoothed) path of the resulting directions.
-    OverviewPolyline Polyline `json:"overview_polyline"`
+	// OverviewPolyline contains an approximate (smoothed) path of the resulting directions.
+	OverviewPolyline Polyline `json:"overview_polyline"`
 
-    // Bounds contains the viewport bounding box of the overview polyline.
-    Bounds Bounds `json:"bounds"`
+	// Bounds contains the viewport bounding box of the overview polyline.
+	Bounds `json:"bounds"`
 
-    // copyrights contains the copyrights text to be displayed for this route. You must handle 
-    // and display this information yourself.
-    Copyrights string `json:"copyrights"`
+	// Copyrights contains the copyrights text to be displayed for this route. You must handle
+	// and display this information yourself.
+	Copyrights string `json:"copyrights"`
 
-    // Warnings contains an array of warnings to be displayed when showing these directions. 
-    // You must handle and display these warnings yourself.
-    Warnings []string `json:"warnings"`
-
+	// Warnings contains an array of warnings to be displayed when showing these directions.
+	// You must handle and display these warnings yourself.
+	Warnings []string `json:"warnings"`
 }
 
 // Bounds represents a bounded area on a map.
 type Bounds struct {
-    // The north east corner of the bounded area.
-    NorthEast LatLng `json:"northeast"`
+	// The north east corner of the bounded area.
+	NorthEast LatLng `json:"northeast"`
 
-    // The south west corner o the bounded area.
-    SouthWest LatLng `json:"southwest"`
+	// The south west corner of the bounded area.
+	SouthWest LatLng `json:"southwest"`
 }
 
 // LatLng represents a location.
 type LatLng struct {
-    // Lat is the latitude of this location.
-    Lat float64 `json:"lat"`
+	// Lat is the latitude of this location.
+	Lat float64 `json:"lat"`
 
-    // Lng is the longitude of this location.
-    Lng float64 `json:"lng"`
+	// Lng is the longitude of this location.
+	Lng float64 `json:"lng"`
 }
 
 // Polyline represents a list of lat,lng points, encoded as a string.
 // See: https://developers.google.com/maps/documentation/utilities/polylinealgorithm
 type Polyline struct {
-    Points string `json:"points"`
+	Points string `json:"points"`
 }
-
 
 // Leg represents a single leg of a route.
 type Leg struct {
-    // Steps contains an array of steps denoting information about each separate step of the 
-    // leg of the journey. 
-    Steps []Step `json:"steps"`
+	// Steps contains an array of steps denoting information about each separate step of the
+	// leg of the journey.
+	Steps []*Step `json:"steps"`
 
-    // Distance indicates the total distance covered by this leg
-    Distance Distance `json:"distance"`
+	// Distance indicates the total distance covered by this leg.
+	Distance `json:"distance"`
 
-    // Duration indicates total time required for this leg
-    Duration Duration `json:"duration"`
+	// Duration indicates total time required for this leg.
+	Duration `json:"duration"`
 
-    // ArrivalTime contains the estimated time of arrival for this leg. This property is only 
-    // returned for transit directions.
-    ArrivalTime *DateTime `json:"arrival_time"`
+	// ArrivalTime contains the estimated time of arrival for this leg. This property is only
+	// returned for transit directions.
+	ArrivalTime *DateTime `json:"arrival_time"`
 
-    // DepartureTime contains the estimated time of departure for this leg. This property is 
-    // only returned for transit directions.
-    DepartureTime *DateTime `json:"departure_time"`
+	// DepartureTime contains the estimated time of departure for this leg. This property is
+	// only returned for transit directions.
+	DepartureTime *DateTime `json:"departure_time"`
 
-    // StartLocation contains the latitude/longitude coordinates of the origin of this leg.
-    StartLocation LatLng `json:"start_location"`
+	// StartLocation contains the latitude/longitude coordinates of the origin of this leg.
+	StartLocation LatLng `json:"start_location"`
 
-    // EndLocation contains the latitude/longitude coordinates of the destination of this leg.
-    EndLocation LatLng `json:"end_location"`
+	// EndLocation contains the latitude/longitude coordinates of the destination of this leg.
+	EndLocation LatLng `json:"end_location"`
 
-    // StartAddress contains the human-readable address (typically a street address) 
-    // reflecting the start location of this leg.
-    StartAddress string `json:"start_address"`    
+	// StartAddress contains the human-readable address (typically a street address)
+	// reflecting the start location of this leg.
+	StartAddress string `json:"start_address"`
 
-    // EndAddress contains the human-readable address (typically a street address) 
-    // reflecting the end location of this leg.
-    EndAddress string `json:"end_address"`
+	// EndAddress contains the human-readable address (typically a street address)
+	// reflecting the end location of this leg.
+	EndAddress string `json:"end_address"`
 }
 
 // Step represents a single step of a leg.
 type Step struct {
-    // html_instructions contains formatted instructions for this step, presented as an HTML text string.
-    HTMLInstructions string `json:"html_instructions"`
+	// html_instructions contains formatted instructions for this step, presented as an HTML text string.
+	HTMLInstructions string `json:"html_instructions"`
 
-    // distance contains the distance covered by this step until the next step. 
-    Distance Distance `json:"distance"`
+	// Distance contains the distance covered by this step until the next step.
+	Distance `json:"distance"`
 
-    // duration contains the typical time required to perform the step, until the next step. 
-    Duration Duration `json:"duration"`
+	// Duration contains the typical time required to perform the step, until the next step.
+	Duration `json:"duration"`
 
-    // StartLocation contains the location of the starting point of this step, as a single set of lat 
-    // and lng fields.
-    StartLocation LatLng `json:"start_location"`
+	// StartLocation contains the location of the starting point of this step, as a single set of lat
+	// and lng fields.
+	StartLocation LatLng `json:"start_location"`
 
-    // end_location contains the location of the last point of this step, as a single set of lat and 
-    // lng fields.
-    EndLocation LatLng `json:"end_location"`
+	// end_location contains the location of the last point of this step, as a single set of lat and
+	// lng fields.
+	EndLocation LatLng `json:"end_location"`
 
-    // polyline contains a single points object that holds an encoded polyline representation of the 
-    // step. This polyline is an approximate (smoothed) path of the step.
-    Polyline Polyline `json:"polyline"`
+	// polyline contains a single points object that holds an encoded polyline representation of the
+	// step. This polyline is an approximate (smoothed) path of the step.
+	Polyline `json:"polyline"`
 
-    // steps contains detailed directions for walking or driving steps in transit directions. Substeps 
-    // are  only available when travel_mode is set to "transit". The inner steps array is of the same 
-    // type as steps.
-    Steps []Step `json:"steps"`
+	// Steps contains detailed directions for walking or driving steps in transit directions. Substeps
+	// are only available when travel_mode is set to "transit". The inner steps array is of the same
+	// type as steps.
+	Steps []*Step `json:"steps"`
 
-    // TransitDetails contains transit specific information. This field is only returned with travel 
-    // mode is set to "transit".
-    TransitDetails TransitDetails `json:"transit_details"`
+	// TransitDetails contains transit specific information. This field is only returned with travel
+	// mode is set to "transit".
+	TransitDetails `json:"transit_details"`
 
-    // TravelMode indicates the travel mode of this step.
-    TravelMode string `json:"travel_mode"`
-    
+	// TravelMode indicates the travel mode of this step.
+	TravelMode string `json:"travel_mode"`
 }
 
 type TransitDetails struct {
-    // TODO(brettmorgan): fill this in
+	// TODO(brettmorgan): fill this in
 }
 
-// This represents a distance covered in a step or leg.
+// Distance represents a distance covered in a step or leg.
 type Distance struct {
-    // Value indicates the distance in meters
-    Value int64 `json:"value"`
+	// Value indicates the distance in meters
+	Value int64 `json:"value"`
 
-    // Text contains a human-readable representation of the distance.
-    Text string `json:"text"`
+	// Text contains a human-readable representation of the distance.
+	Text string `json:"text"`
 }
 
-// This represents a period of time.
+// Duration represents a period of time.
 type Duration struct {
-    // Value indicates the duration in seconds
-    Value int64 `json:"value"`
+	// Value indicates the duration in seconds
+	Value int64 `json:"value"`
 
-    // Text contains a human-readable representation of the duration.
-    Text string `json:"text"`    
+	// Text contains a human-readable representation of the duration.
+	Text string `json:"text"`
 }
 
-// This represents a point in time.
+// DateTime represents a point in time.
 type DateTime struct {
-    // Text the time specified as a string. The time is displayed in the time zone of the 
-    // transit stop.
-    Text string `json:"text"`
+	// Text the time specified as a string. The time is displayed in the time zone of the
+	// transit stop.
+	Text string `json:"text"`
 
-    // TimeZone contains the time zone of this station. The value is the name of the time 
-    // zone as defined in the IANA Time Zone Database, e.g. "America/New_York".
-    TimeZone string `json:"time_zone"`
+	// TimeZone contains the time zone of this station. The value is the name of the time
+	// zone as defined in the IANA Time Zone Database, e.g. "America/New_York".
+	TimeZone string `json:"time_zone"`
 
-    // Value is the number of milliseconds since midnight 01 January, 1970 UTC.
-    Value int64 `json:"value"`
-
+	// Value is the number of milliseconds since midnight 01 January, 1970 UTC.
+	Value int64 `json:"value"`
 }
 
-func Directions(ctx context.Context, origin, destination string) (Response, error) {
-    var response Response
-    req, err := http.NewRequest("GET", "https://maps.googleapis.com/maps/api/directions/json", nil)
-    if err != nil {
-        return response, err
-    }
-    q := req.URL.Query()
-    q.Set("origin", origin)
-    q.Set("destination", destination)
-    q.Set("key", internal.APIKey(ctx))
-    req.URL.RawQuery = q.Encode()
+// Get retrieves directions between the specified origin and destination.
+func Get(ctx context.Context, origin, destination string) (Response, error) {
+	var response Response
+	req, err := http.NewRequest("GET", "https://maps.googleapis.com/maps/api/directions/json", nil)
+	if err != nil {
+		return response, err
+	}
+	q := req.URL.Query()
+	q.Set("origin", origin)
+	q.Set("destination", destination)
+	q.Set("key", internal.APIKey(ctx))
+	req.URL.RawQuery = q.Encode()
 
-    log.Println("Request:", req)
+	log.Println("Request:", req)
 
-    err = httpDo(ctx, req, func(resp *http.Response, err error) error {
-        if err != nil {
-            return err
-        }
-        defer resp.Body.Close()
+	err = httpDo(ctx, req, func(resp *http.Response, err error) error {
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
 
-        if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
-            return err
-        }
-        return nil
-    })
-    // httpDo waits for the closure we provided to return, so it's safe to
-    // read response here.
-    return response, err
+		if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+			return err
+		}
+		return nil
+	})
+	// httpDo waits for the closure we provided to return, so it's safe to
+	// read response here.
+	return response, err
 }
 
 func httpDo(ctx context.Context, req *http.Request, f func(*http.Response, error) error) error {
-    // Run the HTTP request in a goroutine and pass the response to f.
-    tr := &http.Transport{}
-    client := &http.Client{Transport: tr}
-    c := make(chan error, 1)
-    go func() { c <- f(client.Do(req)) }()
-    select {
-    case <-ctx.Done():
-        tr.CancelRequest(req)
-        <-c // Wait for f to return.
-        return ctx.Err()
-    case err := <-c:
-        return err
-    }
+	// Run the HTTP request in a goroutine and pass the response to f.
+	tr := &http.Transport{}
+	client := &http.Client{Transport: tr}
+	c := make(chan error, 1)
+	go func() { c <- f(client.Do(req)) }()
+	select {
+	case <-ctx.Done():
+		tr.CancelRequest(req)
+		<-c // Wait for f to return.
+		return ctx.Err()
+	case err := <-c:
+		return err
+	}
 }
 
 func rawService(ctx context.Context) *http.Client {
-    return internal.Service(ctx, "directions", func(hc *http.Client) interface{} {
-        // TODO(brettmorgan): Introduce a rate limiting wrapper for hc here.
-        return hc
-    }).(*http.Client)
+	return internal.Service(ctx, "directions", func(hc *http.Client) interface{} {
+		// TODO(brettmorgan): Introduce a rate limiting wrapper for hc here.
+		return hc
+	}).(*http.Client)
 }
--- examples/directions/cmdline/main.go
+++ examples/directions/cmdline/main.go
@@ -17,84 +17,72 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "log"
-    "net/http"
-    "os"
-    "time"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
-    "golang.org/x/net/context"
-
-    "google.golang.org/maps"
-    "google.golang.org/maps/directions"
+	"google.golang.org/maps"
+	"google.golang.org/maps/directions"
 )
 
 var (
-    apiKey = flag.String("key", "", "API Key for using Google Maps API.")
-    origin = flag.String("origin", "", "The address or textual latitude/longitude value from which you wish to calculate directions.")
-    destination = flag.String("destination", "", "The address or textual latitude/longitude value from which you wish to calculate directions.")
-)
-
-const (
-    tick = 1 * time.Second
+	apiKey      = flag.String("key", "", "API Key for using Google Maps API.")
+	origin      = flag.String("origin", "", "The address or textual latitude/longitude value from which you wish to calculate directions.")
+	destination = flag.String("destination", "", "The address or textual latitude/longitude value from which you wish to calculate directions.")
 )
 
 func usageAndExit(msg string) {
-    fmt.Fprintln(os.Stderr, msg)
-    fmt.Println("Flags:")
-    flag.PrintDefaults()
-    os.Exit(2)
-}
-
-func getDirections(ctx context.Context, origin, destination string) {
-    d, err := directions.Directions(ctx, origin, destination)
-    if err != nil {
-        log.Fatalf("Directions failed, %v", err)
-    }
-    fmt.Println(d)
+	fmt.Fprintln(os.Stderr, msg)
+	fmt.Println("Flags:")
+	flag.PrintDefaults()
+	os.Exit(2)
 }
 
 func main() {
-    flag.Parse()
-    client := &http.Client{}
-    if *apiKey == "" {
-        usageAndExit("Please specify an API Key.")
-    }
-    if *origin == "" {
-        usageAndExit("Please specify an origin.")
-    }
-    if *destination == "" {
-        usageAndExit("Please specify a destination.")
-    }
-    ctx := maps.NewContext(*apiKey, client)
-    resp, err := directions.Directions(ctx, *origin, *destination)
-    if err != nil {
-        log.Fatalf("Could not request directions: %v", err)
-    }
+	flag.Parse()
+	client := &http.Client{}
+	if *apiKey == "" {
+		usageAndExit("Please specify an API Key.")
+	}
+	if *origin == "" {
+		usageAndExit("Please specify an origin.")
+	}
+	if *destination == "" {
+		usageAndExit("Please specify a destination.")
+	}
+	ctx := maps.NewContext(*apiKey, client)
+	resp, err := directions.Get(ctx, *origin, *destination)
+	if err != nil {
+		log.Fatalf("Could not request directions: %v", err)
+	}
 
-    route := resp.Routes[0]
+  if len(resp.Routes) == 0 {
+    log.Fatalf("No results")
+  }
+	route := resp.Routes[0]
 
-    fmt.Println("Summary:", route.Summary)
-    fmt.Printf("Bounds NorthEast lat/lng: %f,%f\n", route.Bounds.NorthEast.Lat, route.Bounds.NorthEast.Lng)
-    fmt.Printf("Bounds SouthWest lat/lng: %f,%f\n", route.Bounds.SouthWest.Lat, route.Bounds.SouthWest.Lng)
-    fmt.Println("Copyrights:", route.Copyrights)
+	fmt.Println("Summary:", route.Summary)
+	fmt.Printf("Bounds NorthEast lat/lng: %f,%f\n", route.Bounds.NorthEast.Lat, route.Bounds.NorthEast.Lng)
+	fmt.Printf("Bounds SouthWest lat/lng: %f,%f\n", route.Bounds.SouthWest.Lat, route.Bounds.SouthWest.Lng)
+	fmt.Println("Copyrights:", route.Copyrights)
 
-    for idx, leg := range route.Legs {
-        fmt.Println("Leg", idx, "distance:", leg.Distance)
-        fmt.Println("Leg", idx, "duration:", leg.Duration)
-        fmt.Println("Leg", idx, "start address:", leg.StartAddress)
-        fmt.Println("Leg", idx, "start location:", leg.StartLocation)
-        fmt.Println("Leg", idx, "end address:", leg.EndAddress)
-        fmt.Println("Leg", idx, "end location:", leg.EndLocation)
-        
-        for idx, step := range leg.Steps {
-            fmt.Println("Step", idx, "distance:", step.Distance)
-            fmt.Println("Step", idx, "duration:", step.Duration)
-            fmt.Println("Step", idx, "start location:", step.StartLocation)
-            fmt.Println("Step", idx, "end location:", step.EndLocation)
-            fmt.Println("Step", idx, "travel mode:", step.TravelMode)
-        }
-    }
-       
+	for idx, leg := range route.Legs {
+		fmt.Println("Leg", idx, "distance:", leg.Distance)
+		fmt.Println("Leg", idx, "duration:", leg.Duration)
+		fmt.Println("Leg", idx, "start address:", leg.StartAddress)
+		fmt.Println("Leg", idx, "start location:", leg.StartLocation)
+		fmt.Println("Leg", idx, "end address:", leg.EndAddress)
+		fmt.Println("Leg", idx, "end location:", leg.EndLocation)
+
+		for idx, step := range leg.Steps {
+			fmt.Println("Step", idx, "distance:", step.Distance)
+			fmt.Println("Step", idx, "duration:", step.Duration)
+			fmt.Println("Step", idx, "start location:", step.StartLocation)
+			fmt.Println("Step", idx, "end location:", step.EndLocation)
+			fmt.Println("Step", idx, "travel mode:", step.TravelMode)
+		}
+	}
+
 }
