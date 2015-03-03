e828778b6486998743fe9f9aed935c248b0dc3b3
Sam Thorogood <sam.thorogood@gmail.com>
2015-03-03 12:45:00 +1100

    remove goroutine in decode

--- polyline.go
+++ polyline.go
@@ -25,45 +25,20 @@ type Polyline struct {
 	Points []byte `json:"points"`
 }
 
-// decodeString reads int64 values from the encoded source, sending them over
-// the provided channel. This closes the channel when there are no more values.
-func decodeString(s []byte, ch chan int64) {
-	result := int64(1)
-	var shift uint8
-
-	for _, c := range s {
-		b := c - 63 - 1
-		result += int64(b) << shift
-		shift += 5
-		if b >= 0x1f {
-			continue
-		}
-
-		bit := result & 1
-		result >>= 1
-		if bit != 0 {
-			result = ^result
-		}
-		ch <- result
-		result = 1
-		shift = 0
-	}
-	close(ch)
-}
-
 // Decode converts this encoded Polyline to an array of LatLng objects.
 func (p *Polyline) Decode() []LatLng {
-	ch := make(chan int64)
-	go decodeString(p.Points, ch)
+	input := bytes.NewBuffer(p.Points)
 
 	var lat, lng int64
 	path := make([]LatLng, 0, len(p.Points)/2)
 	for {
-		dlat, _ := <-ch
-		dlng, ok := <-ch
-		if !ok {
+		dlat, _ := decodeInt(input)
+		dlng, err := decodeInt(input)
+		if err != nil {
+			// err should only be io.EOF here.
 			return path
 		}
+
 		lat, lng = lat+dlat, lng+dlng
 		path = append(path, LatLng{
 			Lat: float64(lat) * 1e-5,
@@ -71,20 +46,6 @@ func (p *Polyline) Decode() []LatLng {
 		})
 	}
 	panic("should not get here")
-}
-
-// encode writes an encoded int64 to the passed io.ByteWriter.
-func encode(v int64, w io.ByteWriter) {
-	if v < 0 {
-		v = ^(v << 1)
-	} else {
-		v <<= 1
-	}
-	for v >= 0x20 {
-		w.WriteByte((0x20 | (byte(v) & 0x1f)) + 63)
-		v >>= 5
-	}
-	w.WriteByte(byte(v) + 63)
 }
 
 // Encode returns a new encoded Polyline from the given path.
@@ -97,11 +58,52 @@ func Encode(path []LatLng) *Polyline {
 	for _, point := range path {
 		lat, lng := int64(point.Lat*1e5), int64(point.Lng*1e5)
 
-		encode(lat-llat, out)
-		encode(lng-llng, out)
+		encodeInt(lat-llat, out)
+		encodeInt(lng-llng, out)
 
 		llat, llng = lat, lng
 	}
 
 	return &Polyline{Points: out.Bytes()}
+}
+
+// decodeInt reads an encoded int64 from the passed io.ByteReader.
+func decodeInt(r io.ByteReader) (int64, error) {
+	result := int64(1)
+	var shift uint8
+
+	for {
+		raw, err := r.ReadByte()
+		if err != nil {
+			return 0, err
+		}
+
+		b := raw - 63 - 1
+		result += int64(b) << shift
+		shift += 5
+
+		if b < 0x1f {
+			bit := result & 1
+			result >>= 1
+			if bit != 0 {
+				result = ^result
+			}
+			return result, nil
+		}
+	}
+	panic("should not get here")
+}
+
+// encodeInt writes an encoded int64 to the passed io.ByteWriter.
+func encodeInt(v int64, w io.ByteWriter) {
+	if v < 0 {
+		v = ^(v << 1)
+	} else {
+		v <<= 1
+	}
+	for v >= 0x20 {
+		w.WriteByte((0x20 | (byte(v) & 0x1f)) + 63)
+		v >>= 5
+	}
+	w.WriteByte(byte(v) + 63)
 }
