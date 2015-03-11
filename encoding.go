c88106792a4848d48087a0a2970ad58da860a1b5
Brett Morgan <brettmorgan@google.com>
2015-03-11 11:45:24 +1100

    Making maps.go the one place for package comment.

--- encoding.go
+++ encoding.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package maps contains TODO(brettmorgan)
-//
 // More information about Google Distance Matrix API is available on
 // https://developers.google.com/maps/documentation/distancematrix/
 package maps // import "google.golang.org/maps"
