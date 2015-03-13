6dafb4ee1a442e65cdc34a7620811cda9d610de3
Brett Morgan <brettmorgan@google.com>
2015-03-14 09:39:27 +1100

    Adding timezone

---
+++ examples/timezone/cmdline/main.go
@@ -0,0 +1,88 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package main contains a simple command line tool for Timezone API
+// Directions docs: https://developers.google.com/maps/documentation/timezone/
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/kr/pretty"
+	"google.golang.org/maps"
+)
+
+var (
+	apiKey    = flag.String("key", "", "API Key for using Google Maps API.")
+	location  = flag.String("location", "", "a comma-separated lat,lng tuple (eg. location=-33.86,151.20), representing the location to look up.")
+	timestamp = flag.String("timestamp", "", "specifies the desired time as seconds since midnight, January 1, 1970 UTC.")
+	language  = flag.String("language", "", "The language in which to return results.")
+)
+
+func usageAndExit(msg string) {
+	fmt.Fprintln(os.Stderr, msg)
+	fmt.Println("Flags:")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func main() {
+	flag.Parse()
+	client := &http.Client{}
+	if *apiKey == "" {
+		usageAndExit("Please specify an API Key.")
+	}
+	ctx := maps.NewContext(*apiKey, client)
+	r := &maps.TimezoneRequest{
+		Timestamp: *timestamp,
+		Language:  *language,
+	}
+
+	parseLocation(*location, r)
+
+	pretty.Println(r)
+
+	resp, err := r.Get(ctx)
+	if err != nil {
+		log.Fatalf("error %v", err)
+	}
+
+	pretty.Println(resp)
+}
+
+func parseLocation(location string, r *maps.TimezoneRequest) {
+	if location != "" {
+		l := strings.Split(location, ",")
+		lat, err := strconv.ParseFloat(l[0], 64)
+		if err != nil {
+			log.Fatalf("Couldn't parse latlng: %#v", err)
+		}
+		lng, err := strconv.ParseFloat(l[1], 64)
+		if err != nil {
+			log.Fatalf("Couldn't parse latlng: %#v", err)
+		}
+		r.Location = &maps.LatLng{
+			Lat: lat,
+			Lng: lng,
+		}
+	} else {
+		usageAndExit("latlng is required")
+	}
+}
---
+++ timezone.go
@@ -0,0 +1,115 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// More information about Google Distance Matrix API is available on
+// https://developers.google.com/maps/documentation/distancematrix/
+
+package maps // import "google.golang.org/maps"
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+
+	"golang.org/x/net/context"
+	"google.golang.org/maps/internal"
+)
+
+// Get makes a Geocoding API request
+func (r *TimezoneRequest) Get(ctx context.Context) (TimezoneResult, error) {
+	var response timezoneResponse
+
+	req, err := http.NewRequest("GET", "https://maps.googleapis.com/maps/api/timezone/json", nil)
+	if err != nil {
+		return TimezoneResult{}, err
+	}
+	q := req.URL.Query()
+	q.Set("key", internal.APIKey(ctx))
+
+	if r.Location == nil || r.Timestamp == "" {
+		return TimezoneResult{}, errors.New("timezone: You must specify Location and Timestamp")
+	}
+
+	q.Set("location", r.Location.String())
+	q.Set("timestamp", r.Timestamp)
+	if r.Language != "" {
+		q.Set("language", r.Language)
+	}
+
+	req.URL.RawQuery = q.Encode()
+
+	log.Println("Request:", req)
+
+	err = httpDo(ctx, req, func(resp *http.Response, err error) error {
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+
+		if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return TimezoneResult{}, err
+	}
+	if response.Status != "OK" {
+		return TimezoneResult{}, fmt.Errorf("geocoding: %s - %s", response.Status, response.ErrorMessage)
+	}
+
+	return TimezoneResult{
+		DstOffset:    response.DstOffset,
+		RawOffset:    response.RawOffset,
+		TimeZoneID:   response.TimeZoneID,
+		TimeZoneName: response.TimeZoneName,
+	}, nil
+}
+
+// TimezoneRequest is the request structure for Timezone API.
+type TimezoneRequest struct {
+	Location  *LatLng
+	Timestamp string
+	Language  string
+}
+
+type timezoneResponse struct {
+	// DstOffset is the offset for daylight-savings time in seconds.
+	DstOffset int `json:"dstOffset"`
+	// RawOffset is the offset from UTC for the given location.
+	RawOffset int `json:"rawOffset"`
+	// TimeZoneID is a string containing the "tz" ID of the time zone.
+	TimeZoneID string `json:"timeZoneId"`
+	// TimeZoneName is a string containing the long form name of the time zone.
+	TimeZoneName string `json:"timeZoneName"`
+
+	// Status indicating if this request was successful
+	Status string `json:"status"`
+	// ErrorMessage is the explanatory field added when Status is an error.
+	ErrorMessage string `json:"error_message"`
+}
+
+// TimezoneResult is a single geocoded address
+type TimezoneResult struct {
+	// DstOffset is the offset for daylight-savings time in seconds.
+	DstOffset int
+	// RawOffset is the offset from UTC for the given location.
+	RawOffset int
+	// TimeZoneID is a string containing the "tz" ID of the time zone.
+	TimeZoneID string
+	// TimeZoneName is a string containing the long form name of the time zone.
+	TimeZoneName string
+}
