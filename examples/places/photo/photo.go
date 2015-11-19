724108841f9dc8e12e686e2a69fb7235fc7f25f9
Brett Morgan <brettmorgan@google.com>
2015-11-19 13:35:35 +1100

    Start of Places Photo API support.

---
+++ examples/places/photo/photo.go
@@ -0,0 +1,79 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package main contains a simple command line tool for Places API Text Search
+// Documentation: https://developers.google.com/places/web-service/search#TextSearchRequests
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"developers.google.com/maps"
+	"github.com/kr/pretty"
+	"golang.org/x/net/context"
+)
+
+var (
+	apiKey         = flag.String("key", "", "API Key for using Google Maps API.")
+	clientID       = flag.String("client_id", "", "ClientID for Maps for Work API access.")
+	signature      = flag.String("signature", "", "Signature for Maps for Work API access.")
+	photoreference = flag.String("photoreference", "", "Textual identifier that uniquely identifies a place photo.")
+	maxheight      = flag.Int("maxheight", 0, "pecifies the maximum desired height, in pixels, of the image returned by the Place Photos service.")
+	maxwidth       = flag.Int("maxwidth", 0, "pecifies the maximum desired width, in pixels, of the image returned by the Place Photos service.")
+	outfile        = flag.String("out", "", "directory to write image to.")
+)
+
+func usageAndExit(msg string) {
+	fmt.Fprintln(os.Stderr, msg)
+	fmt.Println("Flags:")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func main() {
+	flag.Parse()
+
+	if *outfile == "" {
+		usageAndExit("out directory required")
+	}
+
+	var client *maps.Client
+	var err error
+	if *apiKey != "" {
+		client, err = maps.NewClient(maps.WithAPIKey(*apiKey))
+	} else if *clientID != "" || *signature != "" {
+		client, err = maps.NewClient(maps.WithClientIDAndSignature(*clientID, *signature))
+	} else {
+		usageAndExit("Please specify an API Key, or Client ID and Signature.")
+	}
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+	}
+
+	r := &maps.PlacePhotoRequest{
+		PhotoReference: *photoreference,
+		MaxHeight:      *maxheight,
+		MaxWidth:       *maxwidth,
+	}
+
+	resp, err := client.PlacePhoto(context.Background(), r)
+	if err != nil {
+		log.Fatalf("error %v", err)
+	}
+
+	pretty.Println(resp)
+}
